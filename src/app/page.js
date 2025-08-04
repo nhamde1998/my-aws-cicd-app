d8d8cd2f9bb7cbe3b4253072deb9973c678f63a3
nhamde <narayan.hamde@cctech.co.in>
2025-08-04 17:16:19 +0530

    Initial Commit

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h1>Hello AWS CI/CD 🚀</h1>
+      <p>This site was deployed automatically with AWS CodePipeline</p>
+    </div>
+  );
+}
